0e516a6110f7d4cba725634b58bf4371dd8c206c
Ariful <arifulislamdurjoy3111@gmail.com>
2023-06-09 21:56:57 +0600

    initial commit

---
+++ lib/route/app_page.dart
@@ -0,0 +1,13 @@
+import 'package:get/get.dart';
+import 'package:my_ecommerce/route/app_route.dart';
+import 'package:my_ecommerce/view/dashboard/dashboard_binding.dart';
+import 'package:my_ecommerce/view/dashboard/dashboard_screen.dart';
+
+class AppPage{
+  static var list=[
+    GetPage(
+      name: AppRoute.dashboard, page:()=> DashBoardScreen(),
+      binding: DashboardBinding()
+    )
+  ];
+}
